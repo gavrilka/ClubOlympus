7153ecb79e4e682b4da7a2e10a76f77f5ce836cc
gavrilka <35428739+gavrilka@users.noreply.github.com>
2020-05-05 22:53:58 +0300

    AddMemberActivity created
    Создали разметку для AddMemberActivity. Привязали поля First Name, Last Name, Gender и Group.
    В следующем обновлении добавим значения по умолчанию.

--- app/src/main/java/android/example/clubolympus/AddMemberActivity.java
+++ app/src/main/java/android/example/clubolympus/AddMemberActivity.java
@@ -1,14 +1,28 @@
 package android.example.clubolympus;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AddMemberActivity extends AppCompatActivity {
+
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText groupNameEditText;
+    private Spinner genderSpinner;
+    private int gender = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        groupNameEditText = findViewById(R.id.groupNameEditText);
+        genderSpinner = findViewById(R.id.genderSpinner);
+
     }
 }
