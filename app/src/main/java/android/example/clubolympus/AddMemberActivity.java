77319bc088469964c1f9d15a4f5e24f4d14ff9bb
gavrilka <35428739+gavrilka@users.noreply.github.com>
2020-05-06 21:37:15 +0300

    Save and Delete interface
    Добавлен интерфейс для сохранения и удаления членов клуба.

--- app/src/main/java/android/example/clubolympus/AddMemberActivity.java
+++ app/src/main/java/android/example/clubolympus/AddMemberActivity.java
@@ -1,11 +1,18 @@
 package android.example.clubolympus;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 public class AddMemberActivity extends AppCompatActivity {
 
@@ -31,5 +38,46 @@ public class AddMemberActivity extends AppCompatActivity {
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(spinnerAdapter);
 
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedGender = (String) parent.getItemAtPosition(position);
+                if(!TextUtils.isEmpty(selectedGender)) {
+                    if(selectedGender.equals("Male")){
+                        gender = 1;
+                    } else if (selectedGender.equals("Female")){
+                        gender = 2;
+                    } else {
+                        gender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                gender = 0;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_member:
+                return true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
